fa03e595af043f22a01a0ad656dc3aa533e2a570
Cyrille Bonamy <cyrille.bonamy@legi.cnrs.fr>
2017-04-11 17:56:35 +0200

    add _version.py

---
+++ fluidfoam/_version.py
@@ -0,0 +1,14 @@
+"""
+Module where the version is written.
+
+It is executed in setup.py and imported in fluidfoam/__init__.py.
+
+See:
+
+http://en.wikipedia.org/wiki/Software_versioning
+http://legacy.python.org/dev/peps/pep-0386/
+
+'a' or 'alpha' means alpha version (internal testing),
+'b' or 'beta' means beta version (external testing).
+"""
+__version__ = '0.0.1'
