7b2c9fa79553f2eca2c0fc7064f78a515b52a227
Cyrille Bonamy <cyrille.bonamy@univ-grenoble-alpes.fr>
2019-09-26 00:44:59 +0200

    debug coding style

--- fluidfoam/test_meshdesign.py
+++ fluidfoam/test_meshdesign.py
@@ -6,8 +6,8 @@ import fluidfoam
 class SimpleTestCase(unittest.TestCase):
     def test_meshdesign(self):
         print('meshdesign TestCase')
-        z, dummy, dummy = fluidfoam.getgz(1., 0.001, 20)
-        z, dz, dummy = fluidfoam.getgz(1., 0.1, 20)
-        dz, dummy = fluidfoam.getdzs(1., 0.1, 20)
-        dz, dummy = fluidfoam.getdzs(1., 10, 20)
+        dummy, dummy, dummy = fluidfoam.getgz(1., 0.001, 20)
+        dummy, dummy, dummy = fluidfoam.getgz(1., 0.1, 20)
+        dummy, dummy = fluidfoam.getdzs(1., 0.1, 20)
+        dummy, dummy = fluidfoam.getdzs(1., 10, 20)
 
